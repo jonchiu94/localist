1bcd889766e97ee186ceb8501977efb68ac9073b
harmanbir <harmanbir_19@hotmail.com>
2019-04-30 23:03:43 -0700

    fixed navigation bar

--- localist-frontend/src/components/Home.vue
+++ localist-frontend/src/components/Home.vue
@@ -1,33 +1,33 @@
 <template>
 	<div>
 		<section class="search-sec">
-				<div class="container">
-						<form action="#" method="post" novalidate="novalidate">
-								<div class="row">
-										<div class="col-lg-12">
-												<div class="row">
-														<div class="col-lg-3 col-md-3 col-sm-12 p-0">
-																<input type="text" class="form-control search-slt" placeholder="Enter City">
-														</div>
-														<div class="col-lg-3 col-md-3 col-sm-12 p-0">
-																<input type="text" class="form-control search-slt" placeholder="Enter # of tourists">
-														</div>
-														<div class="col-lg-3 col-md-3 col-sm-12 p-0">
-																<select class="form-control search-slt" id="exampleFormControlSelect1">
-																		<option>Select Tour</option>
-																		<option>Biking</option>
-																		<option>Hiking</option>
-																		<option>Running</option>
-																</select>
-														</div>
-														<div class="col-lg-3 col-md-3 col-sm-12 p-0">
-																<button type="button" class="btn btn-danger wrn-btn">Search</button>
-														</div>
-												</div>
-										</div>
+			<div class="container">
+				<form action="#" method="post" novalidate="novalidate">
+					<div class="row">
+						<div class="col-lg-12">
+							<div class="row">
+								<div class="col-lg-3 col-md-3 col-sm-12 p-0">
+									<input type="text" class="form-control search-slt" placeholder="Enter City">
 								</div>
-						</form>
-				</div>
+								<div class="col-lg-3 col-md-3 col-sm-12 p-0">
+									<input type="text" class="form-control search-slt" placeholder="Enter # of tourists">
+								</div>
+								<div class="col-lg-3 col-md-3 col-sm-12 p-0">
+									<select class="form-control search-slt" id="exampleFormControlSelect1">
+										<option>Select Tour</option>
+										<option>Biking</option>
+										<option>Hiking</option>
+										<option>Running</option>
+									</select>
+								</div>
+								<div class="col-lg-3 col-md-3 col-sm-12 p-0">
+									<button type="button" class="btn btn-danger wrn-btn">Search</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</form>
+			</div>
 		</section>
 	</div>
 </template>
@@ -37,6 +37,9 @@
 </script>
 
 <style>
+.search-sec {
+	margin-top: 200px;
+}
 </style>
 
 
